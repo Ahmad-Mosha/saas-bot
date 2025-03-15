f5ee5da3022091f478d85d6311cb3d2ea727f8c0
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-03-16 01:36:53 +0200

    init bun app

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,9 +1,1 @@
-import { serve } from '@hono/node-server'
-import app from './src/server'
-
-serve({
-  fetch: app.fetch,
-  port: 3001,
-})
-
-console.log('🔥 Hono server running on http://localhost:3001')
+console.log("Hello via Bun!");
